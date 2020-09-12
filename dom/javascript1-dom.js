49d4761da258fccde5ad53fff0b9cc26772b6ab6
skchoi <skchoi@miware.co.kr>
2020-09-12 13:03:28 +0900

    Routine

--- dom/javascript1-dom.js
+++ dom/javascript1-dom.js
@@ -1,12 +1,76 @@
+// Ex8 : 노드 삽입과 바꾸기
+window.addEventListener("load", function() {
+    var section = document.querySelector("#section8");
+    var noticeList = section.querySelector(".notice-list");
+    var tbodyNode = noticeList.querySelector("tbody");
+    var upButton = section.querySelector(".up-button");
+    var downButton = section.querySelector(".down-button");
+
+    var currentNode = tbodyNode.firstElementChild;
+
+    downButton.onclick = function() {
+        var nextNode = currentNode.nextElementSibling;
+        if(nextNode == null) {
+            alert("더 못 내리 갑니다.");
+            return;
+        }
+        // tbodyNode.removeChild(nextNode);
+        tbodyNode.insertBefore(nextNode, currentNode);
+        // currentNode.insertAdjacentElement("beforebegin", nextNode);
+    };
+
+    upButton.onclick = function() {
+        var previousNode = currentNode.previousElementSibling;
+        if(previousNode == null) {
+            alert("더 올라갈 디 없씨유~");
+            return;
+        }
+        //tbodyNode.removeChild(currentNode);
+        tbodyNode.insertBefore(currentNode, previousNode);
+    };
+});
+
 // Ex7 : 노드 복제와 템플릿 태그
 window.addEventListener("load", function() {
     var notices = [
-        {id:5, title:"보험회사", regDate:"2020-04-12", writerId:"Sam", hit:0 },
-        {id:6, title:"제왕의 반지", regDate:"2020-05-22", writerId:"Prodo", hit:8 },
+        {id:5, title:"보험회사", regDate:"2020-04-12", writerId:"Sam", hit:100 },
+        {id:6, title:"제왕의 반지", regDate:"2020-05-22", writerId:"Prodo", hit:200 },
     ];
     
     var section = document.querySelector("#section7");
-   
+    var noticeList = section.querySelector(".notice-list");
+    var tbodyNode = noticeList.querySelector("tbody");
+    var cloneButton = section.querySelector(".clone-button");
+    var templateButton = section.querySelector(".template-button");
+
+    cloneButton.onclick = function() {
+        var trNode = noticeList.querySelector("tbody tr"); // first-child 불필요
+        var cloneNode = trNode.cloneNode(true);
+        var tds = cloneNode.querySelectorAll("td");
+        tds[0].textContent = notices[0].id;
+        tds[1].innerHTML = "<a href='" + notices[0].id + "'>" + notices[0].title + "</a>";
+        tds[2].textContent = notices[0].regDate;
+        tds[3].textContent = notices[0].writerId;
+        tds[4].textContent = notices[0].hit;
+        tbodyNode.append(cloneNode);
+    };
+
+    templateButton.onclick = function() {
+        var template = section.querySelector("template");
+
+        for(var i = 0; i < notices.length; i++) {
+            var cloneNode = document.importNode(template.content, true);
+            var tds = cloneNode.querySelectorAll("td");
+            tds[0].textContent = notices[i].id;
+            var aNode = tds[1].children[0];
+            aNode.href = notices[i].id;
+            aNode.textContent = notices[i].title;
+            tds[2].textContent = notices[i].regDate;
+            tds[3].textContent = notices[i].writerId;
+            tds[4].textContent = notices[i].hit;
+            tbodyNode.append(cloneNode);
+        }
+    };
 });
 
     // Ex6 : 엘리먼트 노드의 속성 & CSS 속성 변경
